3bc83d44d1ba9697a8b19949777bdc29a137a358
Arun <garunalways@gmail.com>
2023-12-17 12:08:01 +0530

    refreshed project

--- README.md
+++ README.md
@@ -1,12 +1,63 @@
-# People in space right now
 
-A simple R shiny app to display the list of astronauts above our sky (in ISS ) right now. The data is fetched from [api.open-notify.org](http://api.open-notify.org/). App is hosted on [shinyapps.io[(https://www.shinyapps.io)
 
-Feel free to fork and modify.
+# People in Space Explorer 🚀
 
-### Do not forget to leave a star! :hugs:
+Welcome to the People in Space Explorer – a dynamic R Shiny app that provides real-time information about astronauts currently in space and the live position of the International Space Station (ISS). Explore the cosmos interactively and learn more about the incredible individuals working beyond our planet.
 
-<a href="https://gpay.app.goo.gl/pay-sCjOGpyBhXM" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A " style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
 
-<br><sup><i>With warm regards,<br>
-**[Arun Gopinath](https:arungopi.gitlab.io)**<i></sup><br>
+## Data Source
+
+The data for this app is sourced from [Open Notify API](http://api.open-notify.org/). It provides live information about the people currently in space and the live position of the International Space Station (ISS).
+
+## Features
+
+- **Live ISS Position Map:** Visualize the current location of the ISS on an interactive map.
+- **Astronaut List with Wikipedia Links:** Browse a list of astronauts on the ISS with hyperlinks to their Wikipedia pages for more details.
+- **Responsive Design:** The app is designed for a seamless experience on various devices.
+
+## Live Demo
+
+Explore the app live on [shinyapps.io](https://arun-gopinath.shinyapps.io/People_space_now/).
+
+## Getting Started
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/arungop/People-in-Space-Explorer.git
+   ```
+
+2. Install required packages:
+
+   ```R
+   install.packages(c("shiny", "leaflet", "shinythemes", "httr", "jsonlite"))
+   ```
+
+3. Run the app:
+
+   ```R
+   library(shiny)
+   runApp("app_directory")
+   ```
+
+## Screenshots
+
+![Screenshot 1](./images/scr_1.jpg)
+![Screenshot 2](./images/scr_2.jpg)
+
+## Contributing
+
+Contributions are welcome! Feel free to submit issues or pull requests.
+
+## About the Developer
+
+Hi there! 👋 I'm [Arun Gopinathan](https://arungopinathan.com), a passionate space enthusiast and developer. Connect with me on [LinkedIn](https://www.linkedin.com/in/aungopinat/) to explore more exciting projects.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+---
+
+**Ready to embark on a cosmic journey?** 🚀✨
+
--- app.R
+++ app.R
@@ -1,32 +1,139 @@
-library(rjson)
+library(shiny)
 library(httr)
 library(jsonlite)
+library(leaflet)
+library(shinydashboard)
+library(shinythemes)
+library(htmlwidgets)
 
-
-# People in space right now
-astro = GET("http://api.open-notify.org/astros.json")
-data = fromJSON(rawToChar(astro$content))
-# Live ISS location
-live = GET("http://api.open-notify.org/iss-now.json")
-now = fromJSON(rawToChar(live$content))
-
-server <- function(input, output) {
-    output$iss <- renderDataTable(data$people)
-    # Generate a summary of the dataset ----
-    output$res <- renderPrint(now$iss_position)
-
+# Function to get Wikipedia link for a given person
+getWikipediaLink <- function(name) {
+  tryCatch({
+    # Search for the Wikipedia page using the person's name
+    wikipedia_url <- paste0("https://en.wikipedia.org/wiki/", gsub(" ", "_", name))
+    # Validate if the Wikipedia page exists
+    if (httr::GET(wikipedia_url)$status_code == 200) {
+      return(wikipedia_url)
+    } else {
+      return(NULL)
+    }
+  }, error = function(e) {
+    return(NULL)
+  })
 }
 
+# People in space right now
+astro <- httr::GET("http://api.open-notify.org/astros.json")
+data <- jsonlite::fromJSON(rawToChar(astro$content))
 
-ui <- basicPage(
-    titlePanel("People in space right now"),
-    dataTableOutput('iss'),
-    # Main panel for displaying outputs ----
-    mainPanel(
+# Live ISS location
+live <- httr::GET("http://api.open-notify.org/iss-now.json")
+now <- jsonlite::fromJSON(rawToChar(live$content))
 
-        # Output: Header + summary of distribution ----
-        h4("Live position of INTERNATIONAL SPACE STATION"),
-        verbatimTextOutput("res"))
+# Convert latitude and longitude to numeric
+now$iss_position$latitude <- as.numeric(now$iss_position$latitude)
+now$iss_position$longitude <- as.numeric(now$iss_position$longitude)
+
+ui <- fluidPage(
+  theme = shinytheme("flatly"),  # Use the flatly theme for a refreshing look
+  dashboardPage(
+    dashboardHeader(title = "People in Space Right Now"),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+        menuItem("About", tabName = "about", icon = icon("info"),
+                 menuSubItem("About the App", tabName = "about_app"),
+                 menuSubItem("About Me", tabName = "about_me")
+        )
+      )
+    ),
+    dashboardBody(
+      fluidRow(
+        # Display total number of people in space in a square box
+        valueBox(
+          value = length(data$people$name),
+          subtitle = "Total People in Space",
+          icon = icon("users"),
+          color = "blue"  # Change 'primary' to 'blue'
+        )
+      ),
+      
+      fluidRow(
+        # Heading for the table
+        h3("List of People in Space", style = "color: #2c3e50;"),
+        # Display the table with hyperlinked names
+        dataTableOutput('iss')
+      ),
+      
+      fluidRow(
+        # Heading for the map
+        h3("Live Position of International Space Station", style = "color: #2c3e50;"),
+        # Display live position of the International Space Station on a map
+        leafletOutput("map"),
+      ),
+      
+      tabItems(
+        tabItem(
+          tabName = "about_app",
+          fluidRow(
+            box(
+              width = 12, solidHeader = TRUE,
+              title = "About the App",
+              status = "info",
+              p("This Shiny app provides real-time information about the people currently in space."),
+              p("Explore the list of astronauts and their positions on the International Space Station.")
+            )
+          )
+        ),
+        tabItem(
+          tabName = "about_me",
+          fluidRow(
+            box(
+              width = 12, solidHeader = TRUE,
+              title = "About Me",
+              status = "info",
+              p("I am a space enthusiast and developer, passionate about bringing information about people in space to the public."),
+              p("Connect with me on LinkedIn:"),
+              a("LinkedIn Profile", href = "https://www.linkedin.com/in/arungopinat/", target="_blank")
+              p("Check out my portfolio"),
+              a("Arun Gopinathan", href = "https://arungopinathan.com", target="_blank")
+            )
+          )
+        )
+      )
+    )
+  )
 )
+
+server <- function(input, output) {
+  # Get Wikipedia links for each person
+  wikipedia_links <- sapply(data$people$name, getWikipediaLink)
+  
+  # Create a new data frame with hyperlinked names
+  hyperlinked_data <- data.frame(
+    name = sapply(seq_along(data$people$name), function(i) {
+      if (!is.null(wikipedia_links[i])) {
+        sprintf('<a href="%s" target="_blank">%s</a>', wikipedia_links[i], data$people$name[i])
+      } else {
+        data$people$name[i]
+      }
+    }),
+    stringsAsFactors = FALSE
+  )
+  
+  output$iss <- renderDataTable(hyperlinked_data, options = list(pageLength = 5), escape = FALSE)
+  
+  # Render ISS live location on a leaflet map
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      setView(lng = now$iss_position$longitude, lat = now$iss_position$latitude, zoom = 8) %>%
+      addMarkers(
+        lng = now$iss_position$longitude,
+        lat = now$iss_position$latitude,
+        popup = sprintf("Latitude: %s<br>Longitude: %s", now$iss_position$latitude, now$iss_position$longitude)
+      )
+  })
+}
 
 shinyApp(ui = ui, server = server)
